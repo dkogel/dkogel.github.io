597fc4048c7ee73facc6ecf57c9705eb404efbca
Daniel Kogel <dkogel123@gmail.com>
2024-11-13 17:49:33 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ window.onloadTurnstileCallback = function () {
     sitekey: "0x4AAAAAAAz93OMmwrC3gdAc",
     callback: function (token) {
       console.log(`Challenge Success ${token}`);
-      setTimeout(() ⇒ {
+      setTimeout(()  {
         document.querySelector(".container").style.display = "flex";
         docucment.getELementbyID("myWidget").style.display = "none";
       }, 2000);
