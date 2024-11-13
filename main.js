25b776a4f1e8cfb9a6ad6fba4039b2f1c5cb0ae7
Daniel Kogel <dkogel123@gmail.com>
2024-11-13 16:49:47 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.onloadTurnstileCallback = function () {
   turnstile.render('#myWidget', {
-    sitekey: '<0x4AAAAAAAz93OMmwrC3gdAc>',
+    sitekey: '0x4AAAAAAAz93OMmwrC3gdAc',
     callback: function (token) {
       console.log(`Challenge Success ${token}`);
       setTimeout(()   {
